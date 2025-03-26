5966b0bc5cec4b36ad0e2b6155a838dff1aa0535
Aleks_Yur <saym101@yandex.ru>
2025-03-26 12:55:38 +0300

    удалено 4 и 12

--- setup.sh
+++ setup.sh
@@ -129,65 +129,9 @@ setup_timezone() {
     fi
 }
 
-# 4. Функция для установки репозиториев и обновления системы
-setup_repositories() {
-    echo "${colors[g]}4] Установка репозиториев и обновление системы${colors[x]}"
-
-    if confirm "${colors[y]}Хотите сменить источник пакетов на xUSSR?${colors[x]}" "n"; then
-        local os_info=$(cat /etc/os-release)
-        local repos=""
-
-        if grep -qi "Debian" <<< "$os_info"; then
-            local debian_ver=$(grep "^VERSION_ID=" <<< "$os_info" | cut -d '"' -f 2)
-            case "$debian_ver" in
-                10) repos='deb http://mirror.docker.ru/debian buster main contrib non-free-firmware
-deb http://mirror.docker.ru/debian-security buster/updates main contrib non-free-firmware
-deb http://mirror.docker.ru/debian buster-updates main contrib non-free-firmware';;
-                11) repos='deb http://mirror.docker.ru/debian bullseye main contrib non-free-firmware
-deb http://mirror.docker.ru/debian bullseye-updates main contrib non-free-firmware
-deb http://mirror.docker.ru/debian-security bullseye-security main contrib non-free-firmware';;
-                12) repos='deb http://mirror.docker.ru/debian bookworm main contrib non-free-firmware
-deb http://mirror.docker.ru/debian-security bookworm-security main contrib non-free-firmware
-deb http://mirror.docker.ru/debian bookworm-updates main contrib non-free-firmware';;
-                13) repos='deb http://mirror.docker.ru/debian trixie main contrib non-free-firmware
-deb http://mirror.docker.ru/debian-security trixie-security main contrib non-free-firmware
-deb http://mirror.docker.ru/debian trixie-updates main contrib non-free-firmware';;
-                *) echo "${colors[r]}Неизвестная версия Debian. Продолжаем без изменений.${colors[x]}";;
-            esac
-        elif grep -qi "Ubuntu" <<< "$os_info"; then
-            local ubuntu_ver=$(grep "^VERSION_ID=" <<< "$os_info" | cut -d '"' -f 2)
-            case "$ubuntu_ver" in
-                22.04) repos='deb http://mirror.yandex.ru/ubuntu/ jammy main restricted universe multiverse
-deb http://mirror.yandex.ru/ubuntu/ jammy-security main restricted universe multiverse
-deb http://mirror.yandex.ru/ubuntu/ jammy-updates main restricted universe multiverse
-deb http://mirror.yandex.ru/ubuntu/ jammy-backports main restricted universe multiverse
-deb http://mirror.yandex.ru/ubuntu/ jammy-proposed main restricted universe multiverse
-deb http://archive.canonical.com/ubuntu/ jammy partner';;
-                20.04) repos='deb http://mirror.yandex.ru/ubuntu/ focal main restricted universe multiverse
-deb http://mirror.yandex.ru/ubuntu/ focal-security main restricted universe multiverse
-deb http://mirror.yandex.ru/ubuntu/ focal-updates main restricted universe multiverse
-deb http://mirror.yandex.ru/ubuntu/ focal-backports main restricted universe multiverse
-deb http://mirror.yandex.ru/ubuntu/ focal-proposed main restricted universe multiverse
-deb http://archive.canonical.com/ubuntu/ focal partner';;
-                *) echo "${colors[r]}Неизвестная версия Ubuntu. Продолжаем без изменений.${colors[x]}";;
-            esac
-        else
-            echo "${colors[r]}Неизвестная операционная система. Продолжаем без изменений.${colors[x]}"
-        fi
-
-        if [[ -n "$repos" ]]; then
-            echo "$repos" > /etc/apt/sources.list
-            echo "${colors[y]}Репозитории успешно обновлены.${colors[x]}"
-            apt update && apt upgrade -y && apt full-upgrade -y && apt autoremove -y
-        fi
-    else
-        echo "${colors[r]}Изменение репозиториев отменено.${colors[x]}"
-    fi
-}
-
-# 5. Функция для установки ПО
+# 4. Функция для установки ПО
 setup_software() {
-    echo "${colors[g]}5] Установка минимального набора ПО${colors[x]}"
+    echo "${colors[g]}4] Установка минимального набора ПО${colors[x]}"
 
     if confirm "${colors[y]}Установить набор стандартных программ?${colors[x]}" "n"; then
         echo "${colors[r]}Список программ для установки:${colors[x]}"
@@ -201,9 +145,9 @@ setup_software() {
     fi
 }
 
-# 6. Функция для настройки Chrony
+# 5. Функция для настройки Chrony
 setup_chrony() {
-    echo "${colors[g]}6] Настройка Chrony${colors[x]}"
+    echo "${colors[g]}5] Настройка Chrony${colors[x]}"
 
     if confirm "${colors[y]}Хотите настроить Chrony?${colors[x]}" "n"; then
         if ! dpkg -s chrony &> /dev/null; then
@@ -222,9 +166,9 @@ setup_chrony() {
     fi
 }
 
-# 7. Функция для настройки доступа root через SSH без пароля с ключом
+# 6. Функция для настройки доступа root через SSH без пароля с ключом
 setup_ssh_keys() {
-    echo "${colors[g]}7] Настройка доступа через SSH-ключи. Не закрывайте текущее окно SSH пока не убедитесь что доступ по ключу работает!!!${colors[x]}"
+    echo "${colors[g]}6] Настройка доступа через SSH-ключи. Не закрывайте текущее окно SSH пока не убедитесь что доступ по ключу работает!!!${colors[x]}"
 
     # Проверка наличия файла authorized_keys
     if [ -f "$authorizedfile" ]; then
@@ -338,9 +282,9 @@ setup_ssh_keys() {
     fi
 }
 
-# 8. Функция для изменения порта SSH
+# 7. Функция для изменения порта SSH
 change_ssh_port() {
-    echo "${colors[g]}8.1] Изменение порта для SSH...${colors[x]}"
+    echo "${colors[g]}7.1] Изменение порта для SSH...${colors[x]}"
     echo
 
     # Получение текущего значения порта SSH
@@ -384,9 +328,9 @@ change_ssh_port() {
     fi
 }
 
-# 8.2 Функция для настройки UFW
+# 7.2 Функция для настройки UFW
 configure_ufw() {
-    echo "${colors[g]}8.2] Первоначальная настройка UFW...${colors[x]}"
+    echo "${colors[g]}7.2] Первоначальная настройка UFW...${colors[x]}"
     echo
 
     # Сброс настроек по умолчанию
@@ -426,9 +370,9 @@ configure_ufw() {
     echo
 }
 
-# 9. Функция для добавления нового пользователя
+# 8. Функция для добавления нового пользователя
 add_new_user() {
-    echo "${colors[g]}9] Добавление пользователя без прав root.${colors[x]}"
+    echo "${colors[g]}8] Добавление пользователя без прав root.${colors[x]}"
     if confirm "Хотите добавить нового пользователя?"; then
         while true; do
             read -r -p "Введите имя пользователя: " new_user
@@ -459,15 +403,15 @@ add_new_user() {
     fi
 }
 
-# 10. Функция для очистки apt кэша
+# 9. Функция для очистки apt кэша
 clean_apt_cache() {
-    echo "${colors[g]}10] Очищаем apt кэш.${colors[x]}"
+    echo "${colors[g]}9] Очищаем apt кэш.${colors[x]}"
     apt clean all && rm -fr /var/cache/*
     echo
     echo "${colors[y]}Кэш очищен${colors[x]}"
 }
 
-# 11. Функция для перезагрузки системы
+# 10. Функция для перезагрузки системы
 reboot_system() {
     echo "${colors[y]}Настройка системы завершена.${colors[x]}"
     echo
@@ -491,64 +435,6 @@ reboot_system() {
     fi
 }
 
-# Функция запускающая выполнение всех пунктов по порядку
-run_all_steps() {
-    setup_hostname
-    echo
-    read -p "${colors[y]}Нажмите Enter для продолжения...${colors[x]}"
-    echo
-
-    setup_locale
-    echo
-    read -p "${colors[y]}Нажмите Enter для продолжения...${colors[x]}"
-    echo
-
-    setup_timezone
-    echo
-    read -p "${colors[y]}Нажмите Enter для продолжения...${colors[x]}"
-    echo
-
-    setup_repositories
-    echo
-    read -p "${colors[y]}Нажмите Enter для продолжения...${colors[x]}"
-    echo
-
-    setup_software
-    echo
-    read -p "${colors[y]}Нажмите Enter для продолжения...${colors[x]}"
-    echo
-
-    setup_chrony
-    echo
-    read -p "${colors[y]}Нажмите Enter для продолжения...${colors[x]}"
-    echo
-
-    setup_ssh_keys
-    echo
-    read -p "${colors[y]}Нажмите Enter для продолжения...${colors[x]}"
-    echo
-
-    local sshport
-    change_ssh_port
-    sshport=$?
-    configure_ufw "$sshport"
-    echo
-    read -p "${colors[y]}Нажмите Enter для продолжения...${colors[x]}"
-    echo
-
-    add_new_user
-    echo
-    read -p "${colors[y]}Нажмите Enter для продолжения...${colors[x]}"
-    echo
-
-    clean_apt_cache
-    echo
-    read -p "${colors[y]}Нажмите Enter для продолжения...${colors[x]}"
-    echo
-
-    reboot_system
-}
-
 # Проверка прав
 if [[ $EUID -ne 0 ]]; then
     echo "${colors[r]}Этот скрипт должен быть запущен от имени root. Перезапустите его 'sudo bash ./script.sh' ${colors[x]}"
@@ -566,16 +452,14 @@ while true; do
     echo "${colors[c]}1.${x}  ${g}Изменить hostname${x}"
     echo "${colors[c]}2.${x}  ${g}Изменить локаль${x}"
     echo "${colors[c]}3.${x}  ${g}Изменить часовой пояс${x}"
-    echo "${colors[c]}4.${x}  ${g}Изменить репозитории и обновить систему${x}"
-    echo "${colors[c]}5.${x}  ${g}Установить ПО${x}"
-    echo "${colors[c]}6.${x}  ${g}Настроить Chrony${x}"
-    echo "${colors[c]}7.${x}  ${g}Настроить SSH ключи${x}"
-    echo "${colors[c]}8.1${x}  ${g}Изменить порт SSH${x}"
-    echo "${colors[c]}8.2${x}  ${g}Настроить UFW${x}"
-    echo "${colors[c]}9.${x}  ${g}Добавить пользователя${x}"
-    echo "${colors[c]}10.${x} ${g}Очистить apt кэш${x}"
-    echo "${colors[c]}11.${x} ${g}Перезагрузить систему${x}"
-    echo "${colors[c]}12.${x} ${g}Запустить выполнение всех пунктов по порядку${x}"
+    echo "${colors[c]}4.${x}  ${g}Установить ПО${x}"
+    echo "${colors[c]}5.${x}  ${g}Настроить Chrony${x}"
+    echo "${colors[c]}6.${x}  ${g}Настроить SSH ключи${x}"
+    echo "${colors[c]}7.1${x}  ${g}Изменить порт SSH${x}"
+    echo "${colors[c]}7.2${x}  ${g}Настроить UFW${x}"
+    echo "${colors[c]}8.${x}  ${g}Добавить пользователя${x}"
+    echo "${colors[c]}9.${x}  ${g}Очистить apt кэш${x}"
+    echo "${colors[c]}10.${x} ${g}Перезагрузить систему${x}"
     echo "${colors[c]}0. Выход${x}"
 
     read -p "${colors[y]}Введите номер:${x} " choice
@@ -584,18 +468,16 @@ while true; do
         1) setup_hostname ;;
         2) setup_locale ;;
         3) setup_timezone ;;
-        4) setup_repositories ;;
-        5) setup_software ;;
-        6) setup_chrony ;;
-        7) setup_ssh_keys ;;
-        8.1) change_ssh_port ;;
-        8.2) 
+        4) setup_software ;;
+        5) setup_chrony ;;
+        6) setup_ssh_keys ;;
+        7.1) change_ssh_port ;;
+        7.2) 
             local sshport=$(grep '^ *Port ' "$sshconfigfile" | awk '{print $2}')
             configure_ufw "$sshport" ;;
-        9) add_new_user ;;
-        10) clean_apt_cache ;;
-        11) reboot_system ;;
-        12) run_all_steps ;;
+        8) add_new_user ;;
+        9) clean_apt_cache ;;
+        10) reboot_system ;;
         0) exit 0 ;;
         *) echo "Неверный выбор. Попробуйте еще раз." ;;
     esac
